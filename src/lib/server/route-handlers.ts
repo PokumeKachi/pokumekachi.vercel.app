a59d9f6af7edd96f16990d976fd907b47026296c
Kachi <108186213+PokumeKachi@users.noreply.github.com>
2026-06-22 19:55:06 +0700

    fix: finish unterminated string

--- src/lib/server/route-handlers.ts
+++ src/lib/server/route-handlers.ts
@@ -5,7 +5,7 @@ export async function handleScriptRoute(event) {
     if (isCurl) {
         const targetUrl =
             "https://gist.githubusercontent.com/PokumeKachi/af266a673d3fe4e66ddd77863140d88a/raw/6bb149d8105939ab5169cedfedc10af1e5db6740/script.sh";
-        const command = `# Script sourced from ${targetUrl}\nTMPFILE=$(mktemp) && curl -L -o "$TMPFILE" "${targetUrl}" && chmod 755 "$TMPFILE" && printf '\n\033[1;32mNext, run \033[1;33m%s\033[1;32m to \033[1;36mexecute\033[1;32m the script!\033[0m\n\n' "$TMPFILE";
+        const command = `# Script sourced from ${targetUrl}\nTMPFILE=$(mktemp) && curl -L -o "$TMPFILE" "${targetUrl}" && chmod 755 "$TMPFILE" && printf '\n\033[1;32mNext, run \033[1;33m%s\033[1;32m to \033[1;36mexecute\033[1;32m the script!\033[0m\n\n' "$TMPFILE"`;
         return new Response(command, {
             status: 200,
             headers: { "Content-Type": "text/plain" },
