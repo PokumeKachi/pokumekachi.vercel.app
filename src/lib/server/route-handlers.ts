6485e0b5c23486b7c1584fb21286e563ee777727
Kachi <108186213+PokumeKachi@users.noreply.github.com>
2026-06-22 19:49:10 +0700

    feat: fix file permissions

--- src/lib/server/route-handlers.ts
+++ src/lib/server/route-handlers.ts
@@ -5,7 +5,7 @@ export async function handleScriptRoute(event) {
     if (isCurl) {
         const targetUrl =
             "https://gist.githubusercontent.com/PokumeKachi/af266a673d3fe4e66ddd77863140d88a/raw/6bb149d8105939ab5169cedfedc10af1e5db6740/script.sh";
-        const command = `# Script sourced from ${targetUrl}\nTMPFILE=$(mktemp) && curl -L -o "$TMPFILE" "${targetUrl}" && chmod +x "$TMPFILE" && echo "Next, run $TMPFILE to execute the script!";`;
+        const command = `# Script sourced from ${targetUrl}\nTMPFILE=$(mktemp) && curl -L -o "$TMPFILE" "${targetUrl}" && chmod 755 "$TMPFILE" && echo "Next, run $TMPFILE to execute the script!";`;
 
         return new Response(command, {
             status: 200,
